cb989beb5f95b72875bf19c34fef9bc9cd64237c
dennis wang <dwang0816@gmail.com>
2019-06-18 18:43:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,3 +16,24 @@ pigeon_data = {
     "City Hall" => ["Andrew"]
   }
 }
+def nyc_pigeon_organizer(data)
+  # write your code here!	  new_hash = {}
+end 	  data.each do |attribute, hash|
+    hash.each do |key, array|
+      array.each do |name|
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
+        if !new_hash[name].has_key?(attribute)
+          new_hash[name][attribute] = []
+        end
+        if !new_hash[name][attribute].include?(key)
+          new_hash[name][attribute] << key.to_s
+        end
+      end
+    end
+  end
+  new_hash
+end
+
+ nyc_pigeon_organizer(pigeon_data)
